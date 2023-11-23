297986a797ab4564eb17d42002eabca2587c1d24
t0dorakis <theodor.hillmannlfunkemedien.de>
2023-11-23 20:57:16 +0100

    additions after first migration

--- migrate.sh
+++ migrate.sh
@@ -1,16 +1,11 @@
-# zsh bash script
-# Migrate zsh config files to home directory
+# show all hidden files
+Defaults write com.apple.Finder AppleShowAllFiles true
 
-cp .zprofile ~/
-cp .zshrc ~/
-source ~/.zshrc
-
-echo "ZSH config files migrated to home directory"
 
 # brew install
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
+eval "$(/opt/homebrew/bin/brew shellenv)"
 # install brew packages
 brew install --cask 1password
 brew install --cask warp
@@ -28,8 +23,23 @@ brew install --cask google-cloud-sdk
 brew install --cask ImageMagick
 brew install awscli
 
+#fix nvm not found issue
+source $(brew --prefix nvm)/nvm.sh
+
+# zsh bash script
+# Migrate zsh config files to home directory
+
+cp .zprofile ~/
+cp .zshrc ~/
+source ~/.zshrc
+
+echo "ZSH config files migrated to home directory"
+
 # correct path for visual studio code CLI
 code version use stable --install-dir "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
 
-#fix nvm not found issue
-source $(brew --prefix nvm)/nvm.sh
+# install node
+nvm install node
+
+# route code cli to visual studio code
+code version use oss --install-dir /Applications/Visual\ Studio\ Code.app
